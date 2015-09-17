1c58ef3a36c2c60707f3fe80263f384e6fca5dc3
Denis Lachance <dudeofea@hotmail.com>
2015-09-17 17:03:07 -0600

    added p font

--- ratt-signal.js
+++ ratt-signal.js
@@ -9,7 +9,7 @@ if (Meteor.isClient) {
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click img': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }

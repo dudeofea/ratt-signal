d594f8c9b066367db5ed98098c449e28d2b45f26
Denis Lachance <dudeofea@hotmail.com>
2015-09-21 17:16:02 -0600

    fixed user accounts (db wasnt loading) and added lightbulb toggle

--- ratt-signal.js
+++ ratt-signal.js
@@ -1,28 +1,50 @@
 AllUsers = new Mongo.Collection('all_users');
 
 if (Meteor.isClient) {
-	//create a new user if doesn't exist
+	var db_ready = false;
 	var user_id = Session.get('user_id');
-	//check if user exists
-	var oid = new Mongo.ObjectID(user_id);
-	if(AllUsers.find(oid).count() < 1){
-		user_id = AllUsers.insert({checked_in: 0});
-		Session.setPersistent('user_id', user_id);
-	}
+	Session.set('light_image', 'signal.png');
+	//wait for db to be ready
+	Meteor.subscribe('db_ready', function(){
+		if(!db_ready){
+			db_ready = true;
+			//get previous user_id
+			//create a new user if doesn't exist
+			if(AllUsers.find({"_id": user_id}).count() < 1){
+				user_id = Random.id();
+				AllUsers.insert({_id: user_id, checked_in: 0});
+				Session.setPersistent('user_id', user_id);
+			}
+		}
+	});
 
 	Template.ratt.helpers({
 		user_count: function () {
-			return AllUsers.find({}).count();
+			return AllUsers.find({checked_in: 1}).count();
+		},
+		light_image: function() {
+			return Session.get('light_image');
 		}
 	});
 
 	Template.ratt.events({
 		'click img': function () {
-			// increment the counter when button is clicked
-			Session.set('counter', Session.get('counter') + 1);
+			//toggle in the user status (and lightbulb)
+			var info = AllUsers.findOne({_id: user_id});
+			if(info['checked_in'] == 0){
+				//turn on
+				Session.set('light_image', 'signal-filled.png');
+			}else{
+				//turn off
+				Session.set('light_image', 'signal.png');
+			}
+			AllUsers.update({_id: user_id}, {checked_in: 1 - info['checked_in']});
 		}
 	});
 }
 
 if (Meteor.isServer) {
+	Meteor.publish('db_ready', function(){
+        return AllUsers.find({});
+    });
 }
